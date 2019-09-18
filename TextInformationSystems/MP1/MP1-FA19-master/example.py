0ab6e3a4719e4b99a47772b6b3c2960fb47e542b
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2019-09-18 17:03:54 +0900

    test code

--- TextInformationSystems/MP1/MP1-FA19-master/example.py
+++ TextInformationSystems/MP1/MP1-FA19-master/example.py
@@ -1,22 +1,42 @@
 import metapy
+import tok as tok
+import trigrams as trigrams
 
 def tokens_lowercase(doc):
-    #Write a token stream that tokenizes with ICUTokenizer, 
-    #lowercases, removes words with less than 2 and more than 5  characters
-    #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
+    # Write a token stream that tokenizes with ICUTokenizer,
+    # lowercases, removes words with less than 2 and more than 5  characters
+    # performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
     '''Place your code here'''
-    
-    #leave the rest of the code as is
+    tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
+    tok = metapy.analyzers.LowercaseFilter(tok)
+    tok = metapy.analyzers.LengthFilter(tok, min=2, max=5)
+    tok = metapy.analyzers.Porter2Filter(tok)
+    ana = metapy.analyzers.NGramWordAnalyzer(3, tok)
+    trigrams = ana.analyze(doc)
+
+    # leave the rest of the code as is
     tok.set_content(doc.content())
     tokens, counts = [], []
     for token, count in trigrams.items():
         counts.append(count)
         tokens.append(token)
     return tokens
-    
+
+def extract_sequences(tok):
+    sequences = []
+    for token in tok:
+        if token == '<s>':
+            sequences.append(metapy.sequence.Sequence())
+        elif token != '</s>':
+            sequences[-1].add_symbol(token)
+    return sequences
+
 if __name__ == '__main__':
+    metapy.log_to_stderr()
     doc = metapy.index.Document()
     doc.content("I said that I can't believe that it only costs $19.95! I could only find it for more than $30 before.")
     print(doc.content()) #you can access the document string with .content()
+
     tokens = tokens_lowercase(doc)
     print(tokens)
+

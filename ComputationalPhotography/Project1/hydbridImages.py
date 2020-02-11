440fb98bc6e946c81290dda01b6043e9372e4403
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2020-02-11 18:04:31 +0900

    add hybrid images

--- ComputationalPhotography/Project1/hydbridImages.py
+++ ComputationalPhotography/Project1/hydbridImages.py
@@ -9,12 +9,59 @@ import utils
 
 print("Hybrid Images.. go go")
 
-im1_file = './Nutmeg.jpg'
+im1_file = './nutmeg.jpg'
 im2_file = './DerekPicture.jpg'
 
 im1 = cv2.imread(im1_file, cv2.IMREAD_GRAYSCALE)
 im2 = cv2.imread(im2_file, cv2.IMREAD_GRAYSCALE)
 
 pts_im1 = utils.prompt_eye_selection(im1)
+pts_im2 = utils.prompt_eye_selection(im2)
+
+im1, im2 = utils.align_images(im1_file, im2_file,pts_im1,pts_im2,save_images=False)
+
+# convert to grayscale
+im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY) / 255.0
+im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY) / 255.0
+
+#Images sanity check
+fig, axes = plt.subplots(1, 2)
+axes[0].imshow(im1,cmap='gray')
+axes[0].set_title('Image 1'), axes[0].set_xticks([]), axes[0].set_yticks([])
+axes[1].imshow(im2,cmap='gray')
+axes[1].set_title('Image 2'), axes[1].set_xticks([]), axes[1].set_yticks([]);
+
+def hybridImage(im1, im2, cutoff_low, cutoff_high):
+    '''
+    Inputs:
+        im1:    RGB (height x width x 3) or a grayscale (height x width) image
+                as a numpy array.
+        im2:    RGB (height x width x 3) or a grayscale (height x width) image
+                as a numpy array.
+        cutoff_low: standard deviation for the low-pass filter
+        cutoff_high: standard deviation for the high-pass filter
+
+    Output:
+        Return the combination of both images, one filtered with a low-pass filter
+        and the other with a high-pass filter.
+    '''
+    print("Hybrid the images")
+    signal.convolve2d()
+
+
+
+arbitrary_value = 20  # you should choose meaningful values; you might want to set to a fraction of image size
+cutoff_low = arbitrary_value
+cutoff_high = arbitrary_value
+
+im_hybrid = hybridImage(im1, im2, cutoff_low, cutoff_high)
+
+# Optional: Select top left corner and bottom right corner to crop image
+# the function returns dictionary of
+# {
+#   'cropped_image': np.ndarray of shape H x W
+#   'crop_bound': np.ndarray of shape 2x2
+# }
+cropped_object = utils.interactive_crop(im_hybrid)
 
 utils.endofProgram()

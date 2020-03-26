eac7d2439ff4706edc59239487e2a78e1744e507
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2020-03-26 14:42:36 +0900

    project 4 initial

---
+++ ComputationalPhotography/ImageBasedLightheing/utils/meta.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+""" Contains meta-class related export functions. """
+
+
+
+def TODO(): 
+    '''
+    Simply helper function to notify student to implement this part of the 
+    solution and raise exception.
+    '''
+    print('Please Implement this!'); 
+    raise NotImplementedError
+    
+def COOL():
+    '''
+    You are cool!
+    Looking through source code is a good habit :D
+    '''
+    TADA = 'good job!'
+    return TADA

45449f305ef7427fefab453c3a98988b7f1d9dcd
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2020-09-22 21:25:39 +0900

    Mp1 part3 done

--- ScientificVisualization/Code/mp1.py
+++ ScientificVisualization/Code/mp1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
-import pdp as ipdb
 
 def getContourCase(top,left,thres,cells):
     row = len(cells)
@@ -31,8 +30,8 @@ def getContourCase(top,left,thres,cells):
     return a 
 
 def interpolate(v1,v2,t):
-    return abs((t-v1)/(v2-v1))
-
+    return (t-v1)/(v2-v1)
+    
 def disambiguateSaddle(top,left,thres,cells):
     if ((cells[left, top] + cells[left+1, top+1] + cells[left, top+1] + cells[left+1, top])/4 >= thres):
         return True
@@ -41,60 +40,60 @@ def disambiguateSaddle(top,left,thres,cells):
     
 def getCellSegments(top,left,thres,cells):
     key = getContourCase(top,left,thres,cells)
-    print("key is ", bin(key))
+#    print("key is ", bin(key))
     
     lines=list([])
 
     if (key&8 == 8 and key&4 == 0): #10xx v1 == 1, v2 == 0
-        v2 = cells[left, top]
-        v1 = cells[left, top+1]
-        t = interpolate(v1, v2, thres)
-        lines.append((left, top+t))
-    elif (key&8 == 0 and key&4 == 4): #01xx v1 == 1, v2 == 0
         v1 = cells[left, top]
-        v2 = cells[left, top+1]
-        t = interpolate(v1, v2, thres)
+        v0 = cells[left, top+1]
+        t = interpolate(v0, v1, thres)
+        lines.append((left, top+(1-t)))
+    elif (key&8 == 0 and key&4 == 4): #01xx v1 == 1, v2 == 0 DONE
+        v0 = cells[left, top]
+        v1 = cells[left, top+1]
+        t = interpolate(v0, v1, thres)
         lines.append((left, top+t))
         
-    if (key&4 == 4 and key&2 == 0): #x10x v1 == 1, v2 == 0
+    if (key&4 == 4 and key&2 == 0): #x10x v1 == 1, v2 == 0 DONE
         v1 = cells[left, top+1]
-        v2 = cells[left+1, top+1]
-        t = interpolate(v1, v2, thres)
-        lines.append((left+t, top+1))
+        v0 = cells[left+1, top+1]
+        t = interpolate(v0, v1, thres)
+        lines.append((left+(1-t), top+1))
     elif (key&4 == 0 and key&2 == 2): #x01x
-        v1 = cells[left, top+1]
-        v2 = cells[left+1, top+1]
-        t = interpolate(v1, v2, thres)
+        v0 = cells[left, top+1]
+        v1 = cells[left+1, top+1]
+        t = interpolate(v0, v1, thres)
         lines.append((left+t, top+1))
     
     if (key&2 == 2 and key&1 == 0): #xx10 v1 == 1, v2 == 0
         v1 = cells[left+1, top+1]
-        v2 = cells[left+1, top]
-        t = interpolate(v1, v2, thres)
+        v0 = cells[left+1, top]
+        t = interpolate(v0, v1, thres)
         lines.append((left+1, top+t))
-    elif (key&2 == 0 and key&1 == 1): #xx01 v1 == 1, v2 == 0
-        v2 = cells[left+1, top+1]
+    elif (key&2 == 0 and key&1 == 1): #xx01 v1 == 1, v2 == 0 DONE
+        v0 = cells[left+1, top+1]
         v1 = cells[left+1, top]
-        t = interpolate(v1, v2, thres)
-        lines.append((left+1, top+t))
+        t = interpolate(v0, v1, thres)
+        lines.append((left+1, top+(1-t)))
             
-    if (key&1 == 0 and key&8 == 8): #1xx0 v1 == 1, v2 == 0
-        v2 = cells[left, top]
-        v1 = cells[left+1, top]
-        t = interpolate(v1, v2, thres)
-        lines.append((left+t, top))
-    elif (key&1 == 1 and key&8 == 0): #0xx1 v1 == 1, v2 == 0
+    if (key&1 == 0 and key&8 == 8): #1xx0 v1 == 1, v2 == 0 
         v1 = cells[left, top]
-        v2 = cells[left+1, top]
-        t = interpolate(v1, v2, thres)
+        v0 = cells[left+1, top]
+        t = interpolate(v0, v1, thres)
+        lines.append((left+(1-t), top))
+    elif (key&1 == 1 and key&8 == 0): #0xx1 v1 == 1, v2 == 0 DONE
+        v0 = cells[left, top]
+        v1 = cells[left+1, top]
+        t = interpolate(v0, v1, thres)
         lines.append((left+t, top))
+
         
     if len(lines) == 0:
         return lines
     else:
         segments = list()
         segments.append(lines)
-#        print(segments)
         return segments
             
 def getContourSegments(thres,cells):
@@ -116,17 +115,18 @@ def getContourSegments(thres,cells):
 #result = getCellSegments(0,0,0.2,test_cases_2)
 #result[0].sort()
 #assert result == [[(0, 0.40876959985875827), (0.40876959985875827, 0)]]
-
-test_cases_3 = np.array([[0.05,0.4,0.4,0.05],[0.4,0.9,0.9,0.4],[0.4,0.9,0.9,0.4],[0.05,0.4,0.4,0.05]])
-result = getCellSegments(2,2,0.2,test_cases_3)
-result[0].sort()
-assert result == [[(2.571428571428571, 3), (3, 2.571428571428571)]]
-
-#test_cases = np.array([[0.07339991, 0.38311005, 0.38311005, 0.07339991],[0.38311005, 0.99744795, 0.99744795, 0.38311005],[0.38311005, 0.99744795, 0.99744795, 0.38311005],[0.07339991, 0.38311005, 0.38311005, 0.07339991]])
-#result = getContourSegments(0.9,test_cases)
-#result.sort()
-#for item in result:
-#    item.sort()
 #
-#assert result == [[(0.8413772778791607, 1), (0.8413772778791607, 2)], [(0.8413772778791607, 1), (1, 0.8413772778791607)], [(0.8413772778791607, 2), (1, 2.1586227221208394)], [(1, 0.8413772778791607), (2, 0.8413772778791607)], [(1, 2.1586227221208394), (2, 2.1586227221208394)], [(2, 0.8413772778791607), (2.1586227221208394, 1)], [(2.1586227221208394, 1), (2.1586227221208394, 2)], [(2, 2.1586227221208394), (2.1586227221208394, 2)]]
-#
+#test_cases_3 = np.array([[0.05,0.4,0.4,0.05],[0.4,0.9,0.9,0.4],[0.4,0.9,0.9,0.4],[0.05,0.4,0.4,0.05]])
+#result = getCellSegments(2,2,0.2,test_cases_3)
+#result[0].sort()
+#assert result == [[(2.571428571428571, 3), (3, 2.571428571428571)]]
+
+test_cases = np.array([[0.07339991, 0.38311005, 0.38311005, 0.07339991],[0.38311005, 0.99744795, 0.99744795, 0.38311005],[0.38311005, 0.99744795, 0.99744795, 0.38311005],[0.07339991, 0.38311005, 0.38311005, 0.07339991]])
+result = getContourSegments(0.9,test_cases)
+result.sort()
+for item in result:
+    item.sort()
+    
+print(result)
+assert result == [[(0.8413772778791607, 1), (0.8413772778791607, 2)], [(0.8413772778791607, 1), (1, 0.8413772778791607)], [(0.8413772778791607, 2), (1, 2.1586227221208394)], [(1, 0.8413772778791607), (2, 0.8413772778791607)], [(1, 2.1586227221208394), (2, 2.1586227221208394)], [(2, 0.8413772778791607), (2.1586227221208394, 1)], [(2.1586227221208394, 1), (2.1586227221208394, 2)], [(2, 2.1586227221208394), (2.1586227221208394, 2)]]
+

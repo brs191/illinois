e9bfeaff00ed70c367f229f57ea9af2540aa128f
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2020-10-29 13:18:23 +0900

    mp2 draft

--- ScientificVisualization/Code/mp2.py
+++ ScientificVisualization/Code/mp2.py
@@ -17,57 +17,55 @@ import numpy as np
 from pyvista import examples
 
 dem = examples.download_crater_topo()
-dem
-
 subset = dem.extract_subset((572, 828, 472, 728, 0, 0), (1,1,1))
-subset
-
 pv.plot_itk(subset)
-
 terrain = subset.warp_by_scalar() #Warp into a 3D surface mesh (without volume)
-print(terrain.dimensions)
-print(terrain)
-
-print(terrain.bounds)
-xmin = terrain.bounds[0]
-xmax = terrain.bounds[1]
-ymin = terrain.bounds[2]
-ymax = terrain.bounds[3]
-zmin = terrain.bounds[4]
-zmax = terrain.bounds[5]
-
-print(xmin)
-print(zmax)
-
-
-terrain.slice()
-
-
-#xnew = np.linspace(xmin, xmax, 126)
-#ynew = np.linspace(ymin, ymax, 126)
-#znew = np.linspace(zmin, zmax, 1)
-#print(len(ynew))
-#
-#x,y,z = np.meshgrid(xnew, ynew, znew)
-#
-#print(y.shape)
-coarse = pv.StructuredGrid()
-coarse.x = terrain.x[0:256:2, 0:256:2, 0:256:2]
-coarse.y = terrain.y[0:256:2, 0:256:2, 0:256:2]
-coarse.z = terrain.z[0:256:2, 0:256:2, 0:256:2]
-
-#print(coarse.dimensions)
-
 #terrain.plot()
 
-#print(terrain.y[0,:,0])
-#print(terrain.x)
-#print(terrain.y)
 
-#print(terrain.z[:2,:2,:2])
-#print(len(terrain.z[:2,:2,:2]))
+xnew = terrain.x[::2, ::2, ::2]
+ynew = terrain.y[::2, ::2, ::2]
+znew = terrain.z[::2, ::2, ::2]
+coarse = pv.StructuredGrid(xnew, ynew, znew)
+#coarse.plot()
+
+#NOTE: You do not need to round any values within your results.
+def bilin(x, y, points):
+    x1, y1, v1 = points[0]
+    x2, y2, v2 = points[1]
+    x3, y3, v3 = points[3]
+    x4, y4, v4 = points[2]
+    
+    tx = (x-x1)/(x2-x1)
+    print(tx)
+    vx = (1-tx)*v1 + tx*v2
+    print(vx)
+    
+    vy = (1-tx)*v4 + tx*v3
+    print(vy)
+    
+    y5 = y1
+    y6 = y4
+    t = (y-y5)/(y6-y5)
+    print(t)
+    v = (1-t)*vx + t*vy
+    print(v)
+    return v
 
 
+errz   = []                    #Create a new empty list for holding absolute error values
+intz   = np.zeros_like(terrain.z) #Create a new array for holding bilinearly interpolated values from coarse mesh
 
-#xrng = np.linspace(X_bounds_min, X_bounds_max, total points), 
-#yrng = np.linspace(Y_bounds_min, Y_bounds_max, total points)
+xlen   = coarse.z.shape[0]-1   #Number of cells (points-1) on the x-axis of the coarse mesh
+ylen   = coarse.z.shape[1]-1   #Number of cells (points-1) on the y-axis of the coarse mesh
+print((xlen)*2)
+print(len(terrain.z))
+scale = (terrain.z.shape[0]-1)/(coarse.z.shape[0]-1) #Reduction factor between original and coarse; should equal 2
+
+# YOUR CODE HERE
+#intmesh = coarse
+#print(coarse.z[1,1,0])\
+print(xnew)
+#for x in range(xlen):
+#    for y in range(ylen):
+#        intz[x][y][0] = bilin(x, y)

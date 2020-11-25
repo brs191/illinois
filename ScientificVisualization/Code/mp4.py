e1288ce042daf7bc2ebb28249164a072d6e48147
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2020-11-25 20:46:17 +0900

    mp4 cs519

--- ScientificVisualization/Code/mp4.py
+++ ScientificVisualization/Code/mp4.py
@@ -4,6 +4,8 @@
 Created on Mon Nov 23 12:55:21 2020
 
 @author: raja
+https://github.com/mpastell/SciPy-CookBook/blob/master/originals/LineIntegralConvolution_attachments/lic_internal.pyx
+https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjctP7utZ3tAhUafd4KHek2B9oQFjAAegQIARAC&url=https%3A%2F%2Fscipy-cookbook.readthedocs.io%2F_downloads%2Fb323b5a79a98fecba57e28185e383c64%2Flic_demo.py&usg=AOvVaw355H3vrH-ib7aFx9ldgWTH
 """
 
 import numpy as np
@@ -31,14 +33,114 @@ for (x,y) in vortices:
     vx +=  (ys-y)/rsq
     vy += -(xs-x)/rsq
     
+
+
+#    - x and y indicate the coordinates of the pixel we're currently on.
+#    - ux and uy indicate the x and y components of the vector at the current pixel
+#    -  dicate the current subpixel position (treating the current pixel
+#      as a unit square where (0,0) represents the top-left of the pixel, while (1,1) 
+#      represents the bottom-right of the pixel).
+#    - nx and ny indicate the total number of pixels along the x and y axes 
+#      within the domain of the entire vector field.
 def advance(ux, uy, x, y, fx, fy, nx, ny):
-    # YOUR CODE HERE
-    for (x,y) in vortices:
-        print(x, y)
-        # for each x,y calculate tx and ty
+
+    if (ux < 0):
+        tx = -fx/ux
+    else:
+        tx = (1-fx)/ux
+        
+    if (uy < 0):
+        ty = -fy/uy
+    else:
+        ty = (1-fy)/uy
+            
+    if tx < ty:
+        if ux >= 0:
+            x += 1
+            fx = 0
+        else:
+            x -= 1
+            fx = 1
+        fy += tx*uy
+    else:
+        if uy >= 0:
+            y += 1
+            fy = 0
+        else:
+            y -= 1
+            fy = 1
+        fx += ty*ux
+        
+# Boundaries round-off
+    if x < 0:
+        x = 0
+    if y < 0:
+        y = 0
+        
+    if x >= nx:
+        x = nx-1
+    if y >= ny:
+        y = ny-1
+        
+    print(x,y,fx,fy)
     
-    return (0,0,0,0)
+    return (x,y,fx,fy)
 
 x_1, y_1, fx_1, fy_1 = advance(-19.09, 14.25, 0, 0, 0.5, 0.5, 10, 10)
 
+#np.testing.assert_allclose(x_1, 0, atol=0.01,rtol=0)
+
 print(x_1, y_1, fx_1, fy_1)
+
+### Please DO NOT hard-code the answers as we will also be using hidden test cases when grading your submission.
+size_test = 100
+
+# Generate 100x100 random noise texture
+np.random.seed(123)
+texture = np.random.rand(size_test, size_test).astype(np.float64)
+
+# Regenerate vector field with new dimensions
+xs = np.linspace(-1,1,size_test).astype(np.float64)[None,:]
+ys = np.linspace(-1,1,size_test).astype(np.float64)[:,None]
+
+vx = np.zeros((size_test,size_test),dtype=np.float64)
+vy = np.zeros((size_test,size_test),dtype=np.float64)
+for (x,y) in vortices:
+    rsq = (xs-x)**2+(ys-y)**2
+    vx +=  (ys-y)/rsq
+    vy += -(xs-x)/rsq
+    
+# Generate sinusoidal kernel function
+L = 5 #Radius of the kernel
+kernel = np.sin(np.arange(2*L+1)*np.pi/(2*L+1)).astype(np.float64)
+
+def compute_streamline(vx, vy, texture, px, py, kernel):
+    # YOUR CODE HERE
+    #You may assume that the shape of vx, vy, and texture are all identical
+    print(vx.shape)
+    print(vy.shape)
+    
+    res = 3.622
+#    Initializing the subpixel position (fx,fy) to the center of the pixel (px,py), where fx and fy function as in advance() above
+    fx = 0.5
+    fy = 0.5
+
+    print(kernel.shape)
+    klen = kernel.shape[0]
+    
+    k = klen//2
+    res = kernel[k]*texture[px, py] #kw*dw
+#    while k>0:
+#        (x, y, fx, fy) = advance(0,0,0,0,0,0,0,0)
+#        k-=1
+#        
+    k = klen//2
+#    while k<klen-1:
+#        (x, y, fx, fy) = advance(0,0,0,0,0,0,0,0)
+#        k+=1
+        
+    return res
+    
+np.testing.assert_allclose(compute_streamline(vx, vy, texture, 9, 9, kernel), 3.622, atol=0.01,rtol=0)
+#np.testing.assert_allclose(compute_streamline(vx, vy, texture, 30, 82, kernel), 5.417, atol=0.01,rtol=0)
+#np.testing.assert_allclose(compute_streamline(vx, vy, texture, 99, 99, kernel), 4.573, atol=0.01,rtol=0)

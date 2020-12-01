84127713b0cdbdd68f4b6140b837f61187b5e2ab
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2020-12-01 15:18:14 +0900

    mp4 not complete

--- ScientificVisualization/Code/mp4.py
+++ ScientificVisualization/Code/mp4.py
@@ -132,27 +132,25 @@ def compute_streamline(vx, vy, texture, px, py, kernel):
             fy = 0.5
             
             k = klen//2
-            res[i,j] += kernel[k] * texture[x,y]
-            while k < klen -1:
-                (x, y, fx, fy) = advance(vx[y,x], vy[y,x], x, y, fx, fy, w, h)
+            res[i,j] += kernel[k]*texture[x,y]
+            while k < klen-1: #forward direction
+                (x, y, fx, fy) = advance(vx[i,j], vy[i,j], x, y, fx, fy, w, h)
                 k += 1
-                res[i,j] += kernel[k] * texture[x,y]
+                res[i,j] += kernel[k]*texture[x,y]
 #                print("+ ", k, " res ", res[i,j])
                 
-            k = klen//2
+#            k = klen//2
             x = j;
             y = i;
             fx = 0.5
             fy = 0.5
-            while k > 0:
-                (x, y, fx, fy) = advance(-vx[y,x], -vy[y,x], x, y, fx, fy, w, h)
+            while k > 0: #backward direction
+                (x, y, fx, fy) = advance(-vx[i,j], -vy[i,j], x, y, fx, fy, w, h)
 #                print("-", k)
                 k -= 1
-                res[i,j] += kernel[k] * texture[x,y]
+                res[i,j] += kernel[k]*texture[x,y]
                 
-            
-
-    
+               
 #    k = klen//2
 #    res = kernel[k]*texture[px, py] #kw*dw
 #    while k>0:
@@ -164,9 +162,57 @@ def compute_streamline(vx, vy, texture, px, py, kernel):
 #    while k<klen-1:
 #        (x, y, fx, fy) = advance(0,0,0,0,0,0,0,0)
 #        k+=1
+
+    print(res)
     return res
     
 #compute_streamline(vx, vy, texture, 9, 9, kernel)
-np.testing.assert_allclose(compute_streamline(vx, vy, texture, 9, 9, kernel), 3.622, atol=0.01,rtol=0)
+#np.testing.assert_allclose(compute_streamline(vx, vy, texture, 9, 9, kernel), 3.622, atol=0.01,rtol=0)
 #np.testing.assert_allclose(compute_streamline(vx, vy, texture, 30, 82, kernel), 5.417, atol=0.01,rtol=0)
 #np.testing.assert_allclose(compute_streamline(vx, vy, texture, 99, 99, kernel), 4.573, atol=0.01,rtol=0)
+
+
+def lic(vx, vy, texture, kernel):
+    # YOUR CODE HERE
+    h = vx.shape[0]
+    w = vx.shape[1]
+    
+    res = np.zeros((h,w))
+    
+    for i in range(h):
+        for j in range(w):
+            px = j
+            py = i;
+            res = compute_streamline(vx, vy, texture, px, py, kernel)
+#            res[j,i] = res/sum(kernel)
+    
+    return res
+
+    
+### Please DO NOT hard-code the answers as we will also be using hidden test cases when grading your submission.
+size_test = 100
+
+# Generate 100x100 random noise texture
+np.random.seed(123)
+texture = np.random.rand(size_test, size_test).astype(np.float64)
+
+# Regenerate vector field with new dimensions
+xs = np.linspace(-1,1,size_test).astype(np.float64)[None,:]
+ys = np.linspace(-1,1,size_test).astype(np.float64)[:,None]
+
+vx = np.zeros((size_test,size_test),dtype=np.float64)
+vy = np.zeros((size_test,size_test),dtype=np.float64)
+for (x,y) in vortices:
+    rsq = (xs-x)**2+(ys-y)**2
+    vx +=  (ys-y)/rsq
+    vy += -(xs-x)/rsq
+    
+# Generate sinusoidal kernel function
+L = 5 #Radius of the kernel
+kernel = np.sin(np.arange(2*L+1)*np.pi/(2*L+1)).astype(np.float64) 
+
+result = lic(vx, vy, texture, kernel)
+
+np.testing.assert_allclose(result[50][50], 0.566, atol=0.01,rtol=0)
+np.testing.assert_allclose(result[99][99], 0.657, atol=0.01,rtol=0)
+np.testing.assert_allclose(result[28][36], 0.405, atol=0.01,rtol=0)
